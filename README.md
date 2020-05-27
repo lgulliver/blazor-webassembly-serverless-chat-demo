d8f96c873212ce55278984609023d462ad1e3f51
Liam Gulliver <liamgulliver@gmail.com>
2020-05-27 17:44:47 +0100

    Initial commit of demo

---
+++ README.md
@@ -0,0 +1,5 @@
+# Blazor WebAssembly Azure Serverless Chat Demo
+
+This is a demo project, showcasing completely serverless chat with Blazor WebAssembly, Azure Functions and Azure SignalR Service.
+
+To run this, you will need an Azure subscription.
---
+++ ServerlessChat.Function/Functions.cs
@@ -0,0 +1,43 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Azure.WebJobs.Extensions.SignalRService;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace ServerlessChat.Functions
+{
+    public static class Functions
+    {
+        [FunctionName("negotiate")]
+        public static SignalRConnectionInfo GetSignalRInfo(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "post")] HttpRequest req,
+            [SignalRConnectionInfo(HubName = "chatHub")] SignalRConnectionInfo connectionInfo)
+        {
+            return connectionInfo;
+        }
+
+        [FunctionName("messages")]
+        public static Task SendMessage(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "post")] ClientMessage clientMessage,
+            [SignalR(HubName = "chatHub")] IAsyncCollector<SignalRMessage> signalRMessages)
+        {
+            return signalRMessages.AddAsync(
+                new SignalRMessage
+                {
+                    Target = "clientMessage",
+                    Arguments = new[] { clientMessage }
+                });
+        }
+    }
+
+    public class ClientMessage
+    {
+        public string Name { get; set; }
+        public string Message { get; set; }
+    }
+}
